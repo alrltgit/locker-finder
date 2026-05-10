f300d72679e4e80f3a47eacba38bae739c94010c
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-05-10 17:18:16 +0200

    fix(lockers_search): fix issue in nearest lockers lookup

--- src/locker_finder/services/lockers_search.py
+++ src/locker_finder/services/lockers_search.py
@@ -10,16 +10,16 @@ def find_nearest_lockers(lockers: list, user_lon: float, user_lat: float) -> lis
     for idx, locker in enumerate(lockers):
         distance = find_distance(user_lon, user_lat, locker["lon"], locker["lat"])
 
-        if len(nearest_lockers) != 3:
+        if len(nearest_lockers) < 5:
             distances.append(distance)
             nearest_lockers.append(locker)
             continue
 
-        min_dist = min(distances)
+        max_dist = max(distances)
 
-        if distance > min_dist:
-            min_dist_idx = distances.index(min_dist)
-            nearest_lockers.pop(min_dist_idx)
-            nearest_lockers.append(locker)
+        if distance < max_dist:
+            max_dist_idx = distances.index(max_dist)
+            nearest_lockers[max_dist_idx] = locker
+            distances[max_dist_idx] = distance
 
     return nearest_lockers
