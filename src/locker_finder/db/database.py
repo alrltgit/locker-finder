eb40bf737902674ff9bd1846a109ad3508188661
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-05-11 17:11:12 +0200

    chore(db): set up sqlmodel integration

---
+++ src/locker_finder/db/database.py
@@ -0,0 +1,33 @@
+"""
+Set up the database connection
+"""
+from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy import text
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://"
+    f"{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@localhost:5432/{POSTGRES_DB}"
+)
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def get_session():
+    return Session(engine)
+
+def create_db_and_tables():
+    with engine.connect() as connection:
+        connection.execute(
+            text("CREATE SCHEMA IF NOT EXISTS locker_finder;")
+        )
+        connection.commit()
+
+    SQLModel.metadata.create_all(engine)
