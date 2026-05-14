19028be39182627dfcaf3e2d2e7df5d48dde74ce
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-05-14 21:25:23 +0200

    chore: remove dotenv

--- src/locker_finder/db/database.py
+++ src/locker_finder/db/database.py
@@ -3,9 +3,6 @@ Set up the database connection
 """
 from sqlmodel import create_engine, Session
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("POSTGRES_URL")
 
