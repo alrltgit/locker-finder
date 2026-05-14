d7c20bcdc04ca92d28bc353b29154d7314dca7e3
Alina Ralcewicz <ralcevichalina70@gmail.com>
2026-05-14 20:55:28 +0200

    chore: add .gitattributes and print statements

--- src/locker_finder/scripts/seed.py
+++ src/locker_finder/scripts/seed.py
@@ -1,4 +1,6 @@
 from src.locker_finder.clients.inpost_client import InPostClient
 
 inpost_client = InPostClient()
+print("\033[32mAdding data to the database. Please wait...\033[0m")
 inpost_client.sync_lockers()
+print("\033[32mData has been added\033[0m")
